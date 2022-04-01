91a81de8da881741060d27e1f7a8df3b09acb097
Maja Berendsen <maja498h@stud.kea.dk>
2022-04-01 17:14:32 +0200

    progress so far

---
+++ script.js
@@ -0,0 +1,29 @@
+let menu = document.getElementById("menu");
+let nav = document.getElementById("navigation");
+let exit = document.getElementById("exit");
+
+const experience = document.querySelector(".ex")
+
+menu.addEventListener("click", function (e) {
+    nav.classList.add("nav-transition");
+    // e.preventDefault();
+  });
+  
+  exit.addEventListener("click", function (e) {
+    nav.classList.remove("nav-transition");
+    // e.preventDefault();
+  });
+  
+  nav.addEventListener("click", function (e) {
+    nav.classList.remove("nav-transition");
+  });
+  
+
+  function openNav() {
+    document.getElementById("slideNav").style.width = "1000px";
+  }
+  
+  function closeNav() {
+    document.getElementById("slideNav").style.width = "0";
+  }
+  
