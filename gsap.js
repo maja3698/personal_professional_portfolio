94bf40947de8a3aeb930da7125a383371bc29b70
Maja Berendsen <maja498h@stud.kea.dk>
2022-05-22 16:48:55 +0200

    added go to prev and next

--- gsap.js
+++ gsap.js
@@ -1,75 +1,109 @@
 gsap.registerPlugin(ScrollTrigger);
 
-
-
 gsap.to(".free-fridges-w", {
-  x:700,
-  duration: 1,
+  x: 700,
+  duration: 3,
   scrollTrigger: {
-      trigger: ".free-fridges-w",
-      start: "top 70%",
-      end: "center 95%",
-      scrub:4,
-      toggleActions: "restart none none none",
+    trigger: ".free-fridges-w",
+    start: "top 95%",
+    end: "center 95%",
+    scrub: 4,
+    toggleActions: "restart none none none",
+    // markers: {
+    //     startColor: "purple",
+    //     endColor: "black",
+    //     fontSize: "1rem",
+    //     indent: 200
+    // }
+  },
+});
+
+gsap.to(".louisiana-w", {
+  x: -700,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".free-fridges-w",
+    start: "top 95%",
+    end: "center 95%",
+    scrub: 4,
+    toggleActions: "restart none none none",
+    // markers: {
+    //     startColor: "purple",
+    //     endColor: "black",
+    //     fontSize: "1rem",
+    //     indent: 200
+    // }
+  },
+});
+
+gsap.to(".quickit-w", {
+  x: 700,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".quickit-w",
+    start: "top 95%",
+    end: "center 95%",
+    scrub: 4,
+    toggleActions: "restart none none none",
+    // markers: {
+    //     startColor: "purple",
+    //     endColor: "black",
+    //     fontSize: "1rem",
+    //     indent: 200
+    // }
+  },
+});
+
+gsap.to(".hogwarts-w", {
+  x: -700,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".quickit-w",
+    start: "top 95%",
+    end: "center 90%",
+    scrub: 4,
+    toggleActions: "restart none none none",
     //   markers: {
     //       startColor: "purple",
     //       endColor: "black",
     //       fontSize: "1rem",
     //       indent: 200
     //   }
-  }
-})
+  },
+});
 
-gsap.to(".louisiana-w", {
-    x:-700,
-    duration: 1,
-    scrollTrigger: {
-        trigger: ".free-fridges-w",
-        start: "top 70%",
-        end: "center 95%",
-        scrub: 4,
-        toggleActions: "restart none none none",
-        // markers: {
-        //     startColor: "purple",
-        //     endColor: "black",
-        //     fontSize: "1rem",
-        //     indent: 200
-        // }
-    }
-  })
+gsap.to(".quick2it-w", {
+  x: 700,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".quick2it-w",
+    start: "top 95%",
+    end: "center 95%",
+    scrub: 4,
+    toggleActions: "restart none none none",
+    // markers: {
+    //     startColor: "purple",
+    //     endColor: "black",
+    //     fontSize: "1rem",
+    //     indent: 200
+    // }
+  },
+});
 
-  gsap.to(".quickit-w", {
-    x:700,
-    duration: 1,
-    scrollTrigger: {
-        trigger: ".quickit-w",
-        start: "top 70%",
-        end: "center 95%",
-        scrub:4,
-        toggleActions: "restart none none none",
-      //   markers: {
-      //       startColor: "purple",
-      //       endColor: "black",
-      //       fontSize: "1rem",
-      //       indent: 200
-      //   }
-    }
-  })
-  
-  gsap.to(".hogwarts-w", {
-      x:-700,
-      duration: 1,
-      scrollTrigger: {
-          trigger: ".quickit-w",
-          start: "top 70%",
-          end: "center 95%",
-          scrub: 4,
-          toggleActions: "restart none none none",
-        //   markers: {
-        //       startColor: "purple",
-        //       endColor: "black",
-        //       fontSize: "1rem",
-        //       indent: 200
-        //   }
-      }
-    })
+gsap.to(".silfen-w", {
+  x: -700,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".quick2it-w",
+    start: "top 95%",
+    end: "center 90%",
+    scrub: 4,
+    toggleActions: "restart none none none",
+    //   markers: {
+    //       startColor: "purple",
+    //       endColor: "black",
+    //       fontSize: "1rem",
+    //       indent: 200
+    //   }
+  },
+});
