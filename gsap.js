b45fc9bb7f2fc1babb0959cc3b742886cc040eb1
Maja Berendsen <maja498h@stud.kea.dk>
2022-05-24 17:29:10 +0200

    responsive landing page

--- gsap.js
+++ gsap.js
@@ -1,109 +1,120 @@
+
+
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".free-fridges-w", {
-  x: 700,
-  duration: 3,
-  scrollTrigger: {
-    trigger: ".free-fridges-w",
-    start: "top 95%",
-    end: "center 95%",
-    scrub: 4,
-    toggleActions: "restart none none none",
-    // markers: {
-    //     startColor: "purple",
-    //     endColor: "black",
-    //     fontSize: "1rem",
-    //     indent: 200
-    // }
-  },
-});
+ScrollTrigger.matchMedia({
+  "(min-width: 1065px)": function(){
+    console.log("Desktop")
+    gsap.to(".free-fridges-w", {
+      x: 1219,
+      duration: 3,
+      scrollTrigger: {
+        trigger: ".free-fridges-w",
+        start: "top 95%",
+        end: "center 95%",
+        scrub: 4,
+        toggleActions: "restart none none none",
+        // markers: {
+        //     startColor: "purple",
+        //     endColor: "black",
+        //     fontSize: "1rem",
+        //     indent: 200
+        // }
+      },
+    });
+    
+    gsap.to(".louisiana-w", {
+      x: -1219,
+      duration: 3,
+      scrollTrigger: {
+        trigger: ".free-fridges-w",
+        start: "top 95%",
+        end: "center 95%",
+        scrub: 4,
+        toggleActions: "restart none none none",
+        // markers: {
+        //     startColor: "purple",
+        //     endColor: "black",
+        //     fontSize: "1rem",
+        //     indent: 200
+        // }
+      },
+    });
+    
+    gsap.to(".quickit-w", {
+      x: 1219,
+      duration: 3,
+      scrollTrigger: {
+        trigger: ".quickit-w",
+        start: "top 95%",
+        end: "center 95%",
+        scrub: 4,
+        toggleActions: "restart none none none",
+        // markers: {
+        //     startColor: "purple",
+        //     endColor: "black",
+        //     fontSize: "1rem",
+        //     indent: 200
+        // }
+      },
+    });
+    
+    gsap.to(".hogwarts-w", {
+      x: -1219,
+      duration: 3,
+      scrollTrigger: {
+        trigger: ".quickit-w",
+        start: "top 95%",
+        end: "center 90%",
+        scrub: 4,
+        toggleActions: "restart none none none",
+        //   markers: {
+        //       startColor: "purple",
+        //       endColor: "black",
+        //       fontSize: "1rem",
+        //       indent: 200
+        //   }
+      },
+    });
+    
+    gsap.to(".quick2it-w", {
+      x: 1219,
+      duration: 3,
+      scrollTrigger: {
+        trigger: ".quick2it-w",
+        start: "top 95%",
+        end: "center 95%",
+        scrub: 4,
+        toggleActions: "restart none none none",
+        // markers: {
+        //     startColor: "purple",
+        //     endColor: "black",
+        //     fontSize: "1rem",
+        //     indent: 200
+        // }
+      },
+    });
+    
+    gsap.to(".silfen-w", {
+      x: -1219,
+      duration: 3,
+      scrollTrigger: {
+        trigger: ".quick2it-w",
+        start: "top 95%",
+        end: "center 90%",
+        scrub: 4,
+        toggleActions: "restart none none none",
+        //   markers: {
+        //       startColor: "purple",
+        //       endColor: "black",
+        //       fontSize: "1rem",
+        //       indent: 200
+        //   }
+      },
+    });
 
-gsap.to(".louisiana-w", {
-  x: -700,
-  duration: 3,
-  scrollTrigger: {
-    trigger: ".free-fridges-w",
-    start: "top 95%",
-    end: "center 95%",
-    scrub: 4,
-    toggleActions: "restart none none none",
-    // markers: {
-    //     startColor: "purple",
-    //     endColor: "black",
-    //     fontSize: "1rem",
-    //     indent: 200
-    // }
-  },
-});
+  }
+}
 
-gsap.to(".quickit-w", {
-  x: 700,
-  duration: 3,
-  scrollTrigger: {
-    trigger: ".quickit-w",
-    start: "top 95%",
-    end: "center 95%",
-    scrub: 4,
-    toggleActions: "restart none none none",
-    // markers: {
-    //     startColor: "purple",
-    //     endColor: "black",
-    //     fontSize: "1rem",
-    //     indent: 200
-    // }
-  },
-});
-
-gsap.to(".hogwarts-w", {
-  x: -700,
-  duration: 3,
-  scrollTrigger: {
-    trigger: ".quickit-w",
-    start: "top 95%",
-    end: "center 90%",
-    scrub: 4,
-    toggleActions: "restart none none none",
-    //   markers: {
-    //       startColor: "purple",
-    //       endColor: "black",
-    //       fontSize: "1rem",
-    //       indent: 200
-    //   }
-  },
-});
-
-gsap.to(".quick2it-w", {
-  x: 700,
-  duration: 3,
-  scrollTrigger: {
-    trigger: ".quick2it-w",
-    start: "top 95%",
-    end: "center 95%",
-    scrub: 4,
-    toggleActions: "restart none none none",
-    // markers: {
-    //     startColor: "purple",
-    //     endColor: "black",
-    //     fontSize: "1rem",
-    //     indent: 200
-    // }
-  },
-});
-
-gsap.to(".silfen-w", {
-  x: -700,
-  duration: 3,
-  scrollTrigger: {
-    trigger: ".quick2it-w",
-    start: "top 95%",
-    end: "center 90%",
-    scrub: 4,
-    toggleActions: "restart none none none",
-    //   markers: {
-    //       startColor: "purple",
-    //       endColor: "black",
-    //       fontSize: "1rem",
-    //       indent: 200
-    //   }
-  },
-});
+ 
+)
--- script.js
+++ script.js
@@ -1,34 +1,63 @@
-import AOS from 'aos';
-AOS.init();
+// import AOS from 'aos';
+// AOS.init();
 
 let menu = document.getElementById("menu");
 let nav = document.getElementById("navigation");
 let exit = document.getElementById("exit");
 
+let desktop = window.matchMedia("(min-width: 1234px)")
 
+const header = document.querySelector(".leftheader")
 
 menu.addEventListener("click", function (e) {
-    nav.classList.add("nav-transition");
-    // e.preventDefault();
-  });
-  
-  exit.addEventListener("click", function (e) {
-    nav.classList.remove("nav-transition");
-    // e.preventDefault();
-  });
-  
-  nav.addEventListener("click", function (e) {
-    nav.classList.remove("nav-transition");
-  });
-  
+  nav.classList.add("nav-transition");
+  // e.preventDefault();
+});
 
-  function openNav() {
-    document.getElementById("slideNav").style.width = "1000px";
-  }
-  
-  function closeNav() {
-    document.getElementById("slideNav").style.width = "0";
-  }
- 
+exit.addEventListener("click", function (e) {
+  nav.classList.remove("nav-transition");
+  // e.preventDefault();
+});
 
-  
+nav.addEventListener("click", function (e) {
+  nav.classList.remove("nav-transition");
+});
+
+
+
+const keyframes = [
+  {
+    height: "30vh",
+    easing: "ease-in",
+  },
+  {
+    height: "100vh",
+    easing: "ease-in",
+  },
+];
+
+const properties = {
+  duration: 750,
+  delay: 2,
+  iterations: 1,
+};
+
+
+if (desktop.matches){
+  header.animate(keyframes, properties);
+
+}
+function openNav() {
+  document.getElementById("slideNav").style.width = "1000px";
+}
+
+function closeNav() {
+  document.getElementById("slideNav").style.width = "0";
+}
+
+
+
+
+
+
+
